be79ec819f67666f411745ebf3547898e9b74cc9
Scott Mayberry <sctmayberry@gmail.com>
2022-05-31 14:08:24 -0400

    added detect batch

--- Goertzel.cpp
+++ Goertzel.cpp
@@ -1,13 +1,13 @@
 /*
-  The Goertzel algorithm is long standing so see 
+  The Goertzel algorithm is long standing so see
   http://en.wikipedia.org/wiki/Goertzel_algorithm for a full description.
-  It is often used in DTMF tone detection as an alternative to the Fast 
+  It is often used in DTMF tone detection as an alternative to the Fast
   Fourier Transform because it is quick with low overheard because it
-  is only searching for a single frequency rather than showing the 
+  is only searching for a single frequency rather than showing the
   occurrence of all frequencies.
-  
+
   This work is entirely based on the Kevin Banks code found at
-  http://www.embedded.com/design/configurable-systems/4024443/The-Goertzel-Algorithm 
+  http://www.embedded.com/design/configurable-systems/4024443/The-Goertzel-Algorithm
   so full credit to him for his generic implementation and breakdown. I've
   simply massaged it into an Arduino library. I recommend reading his article
   for a full description of whats going on behind the scenes.
@@ -21,44 +21,34 @@
 // include this library's description file
 #include "Goertzel.h"
 
-Goertzel::Goertzel(float in_TARGET_FREQUENCY, float in_SAMPLING_FREQUENCY, int in_ADCCENTER)
+Goertzel::Goertzel(float in_TARGET_FREQUENCY, float in_SAMPLING_FREQUENCY, int n)
 {
-  initialize(in_TARGET_FREQUENCY, in_SAMPLING_FREQUENCY, in_ADCCENTER);
-}
-
-Goertzel::Goertzel(float in_TARGET_FREQUENCY, float in_SAMPLING_FREQUENCY)
-{
-  initialize(in_TARGET_FREQUENCY, in_SAMPLING_FREQUENCY, 128);
+  initialize(in_TARGET_FREQUENCY, in_SAMPLING_FREQUENCY, n);
 }
 
 Goertzel::Goertzel()
 {
-  initialize(24000, 240000, 128);
+  initialize(24000, 100000, 100);
 }
 
-void Goertzel::initialize(float in_TARGET_FREQUENCY, float in_SAMPLING_FREQUENCY, int in_ADCCENTER)
+void Goertzel::initialize(float in_TARGET_FREQUENCY, float in_SAMPLING_FREQUENCY, int n)
 {
-  SAMPLING_FREQUENCY = in_SAMPLING_FREQUENCY; //on 16mhz, ~8928.57142857143, on 8mhz ~44444
-  TARGET_FREQUENCY = in_TARGET_FREQUENCY;     //should be integer of SAMPLING_RATE/N
-  ADCCENTER = in_ADCCENTER;
-  N = 0;
+  SAMPLING_FREQUENCY = in_SAMPLING_FREQUENCY; // on 16mhz, ~8928.57142857143, on 8mhz ~44444
+  TARGET_FREQUENCY = in_TARGET_FREQUENCY;     // should be integer of SAMPLING_RATE/N
+  N = n;
 
+  // pre-calc the coeifficents for the goertzel
   float omega = (2.0 * PI * TARGET_FREQUENCY) / SAMPLING_FREQUENCY;
-
+  cosine = cos(omega);
   coeff = 2.0 * cos(omega);
-  sinePart = sin(omega);
+  sine = sin(omega);
 
   ResetGoertzel();
 }
 
-void Goertzel::reinit(float in_TARGET_FREQUENCY, float in_SAMPLING_FREQUENCY)
+void Goertzel::reinit(float in_TARGET_FREQUENCY, float in_SAMPLING_FREQUENCY, int n)
 {
-  initialize(in_TARGET_FREQUENCY, in_SAMPLING_FREQUENCY, 128);
-}
-
-void Goertzel::reinit(float in_TARGET_FREQUENCY, float in_SAMPLING_FREQUENCY, int in_ADCCENTER)
-{
-  initialize(in_TARGET_FREQUENCY, in_SAMPLING_FREQUENCY, in_ADCCENTER);
+  initialize(in_TARGET_FREQUENCY, in_SAMPLING_FREQUENCY, n);
 }
 
 float Goertzel::getSampleFreq()
@@ -76,8 +66,6 @@ void Goertzel::ResetGoertzel()
 {
   Q2 = 0;
   Q1 = 0;
-  sumOfSquares = 0;
-  readIndex = 0;
 }
 
 float Goertzel::applyHammingWindow(float sample)
@@ -91,61 +79,17 @@ float Goertzel::applyExactBlackman(float sample)
 }
 
 /* Call this routine for every sample. */
-void Goertzel::ProcessSample(float zero_centered_sample)
+void Goertzel::addSample(float zero_centered_scaled_sample)
 {
   float Q0;
-  Q0 = coeff * Q1 - Q2 + zero_centered_sample;
+  Q0 = coeff * Q1 - Q2 + zero_centered_scaled_sample;
   Q2 = Q1;
   Q1 = Q0;
 }
 
-void Goertzel::ProcessSample(int zero_centered_sample)
+void Goertzel::setN(int newN)
 {
-  ProcessSample(float(zero_centered_sample));
-}
-
-int Goertzel::getSampleIndex()
-{
-  return readIndex;
-}
-
-bool Goertzel::addSampleWithCheck(int sample, int maxN)
-{
-  if (readIndex < maxN)
-  {
-    addSample(sample);
-    return true;
-  }
-  return false;
-}
-
-void Goertzel::setN(int n)
-{
-  N = n;
-}
-
-void Goertzel::addSample(int sample)
-{
-  sample = sample - ADCCENTER;
-  float adjustedSample;
-  if ((hamming || exactBlackman) && N != 0)
-  {
-    if (hamming)
-    {
-      adjustedSample = sample * applyHammingWindow(sample);
-    }
-    else
-    {
-      adjustedSample = sample * applyExactBlackman(sample);
-    }
-  }
-  else
-  {
-    adjustedSample = sample;
-  }
-  sumOfSquares += sample * sample;
-  ProcessSample(sample);
-  readIndex++;
+  N = newN;
 }
 
 void Goertzel::setHamming(bool hamm)
@@ -160,33 +104,58 @@ void Goertzel::setExactBlackman(bool eb)
 
 float Goertzel::calcMagnitudeSquared()
 {
+  // Serial.println(Q1 * Q1);
+  // Serial.println(Q2 * Q2);
+  // Serial.println(coeff * Q1 * Q2);
   return Q1 * Q1 + Q2 * Q2 - coeff * Q1 * Q2;
 }
 
-float Goertzel::calcPurity(float magnitudesquared, int n)
+float Goertzel::calcPurity()
 {
-  return (2 * magnitudesquared) / (n * sumOfSquares);
+  return (2 * calcMagnitudeSquared() / N);
 }
 
-float Goertzel::detect()
+float Goertzel::calcPurityWithDifferentN(int numberOfSamples)
 {
-  float purity = calcPurity(calcMagnitudeSquared(), readIndex);
+  return (2 * calcMagnitudeSquared() / numberOfSamples);
+}
+
+float Goertzel::calcRealPart()
+{
+  return Q1 - Q2 * cosine;
+}
+
+float Goertzel::calcImagPart()
+{
+  return Q2 * sine;
+}
+
+float Goertzel::detectWithDifferentN(int numberOfSamples)
+{
+  float purity = calcPurityWithDifferentN(numberOfSamples);
   ResetGoertzel();
   return purity;
 }
 
-float Goertzel::detectBatch(byte samples[], int n)
+float Goertzel::detect()
+{
+  float purity = calcPurity();
+  ResetGoertzel();
+  return purity;
+}
+
+float Goertzel::detectBatch(float samples[], int sizeOfSamples, int startIndex)
 {
   ResetGoertzel();
-  testData = samples;
-
+  // save previous N for resetting after batch detect
   /* Process the samples. */
-  for (int index = 0; index < n; index++)
+
+  for (int index = 0; index < sizeOfSamples; index++)
   {
-    addSample(testData[index]);
+    addSample(samples[(index + startIndex) % sizeOfSamples]);
   }
 
-  float purity = calcPurity(calcMagnitudeSquared(), n);
-  ResetGoertzel();
+  float purity = detectWithDifferentN(sizeOfSamples);
+  // Serial.println(microsfour - micros());
   return purity;
 }
