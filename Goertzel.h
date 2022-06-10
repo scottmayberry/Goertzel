2cc5b083d563ef7efe976c1cf2787954a6d0545c
Scott Mayberry <sctmayberry@gmail.com>
2022-06-10 15:38:46 -0400

    changed reset goertzel to public

--- Goertzel.h
+++ Goertzel.h
@@ -48,11 +48,11 @@ public:
 
   void setHamming(bool);
   void setExactBlackman(bool);
+  void ResetGoertzel();
 
   // library-accessible "private" interface
 private:
   void initialize(float, float, int);
-  void ResetGoertzel();
 
   float calcMagnitudeSquared();
   float calcPurity();
